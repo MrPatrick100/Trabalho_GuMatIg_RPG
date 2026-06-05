ef6cdc12e5bb987140c163615c3e771021e5cf67
MrPatrick100 <igorxmf.cp2@gmail.com>
2026-06-04 23:11:43 -0300

    add Relação Personagem class

--- repository/RelacaoPersonagemHabilidadeRepository.php
+++ repository/RelacaoPersonagemHabilidadeRepository.php
@@ -12,64 +12,61 @@ class RelacaoPersonagemHabilidadeRepository {
     }
 
     /** @return RelacaoPersonagemHabilidade[] */ // Lista todas as habilidades salvas no Banco daquele usuário
-    // public function listarPorUsuario(int $usuarioId): array {
-    //     $stmt = $this->pdo->prepare(
-    //         'SELECT * FROM habilidade WHERE id_usuario = :idu ORDER BY nome ASC'
-    //     );
-    //     $stmt->execute([':idu' => $usuarioId]);
-    //     $lista = [];
-    //     foreach ($stmt->fetchAll() as $dados) {
-    //         $lista[] = new Habilidade($dados);
-    //     }
-    //     return $lista;
-    // }
+    public function listarPorUsuario(int $usuarioId): array {
+        $stmt = $this->pdo->prepare(
+            'SELECT * FROM personagem_habilidade WHERE id_usuario = :idu ORDER BY nome ASC'
+        );
+        $stmt->execute([':idu' => $usuarioId]);
+        $lista = [];
+        foreach ($stmt->fetchAll() as $dados) {
+            $lista[] = new RelacaoPersonagemHabilidade($dados);
+        }
+        return $lista;
+    }
 
-    // public function buscarPorId(int $id): ?Habilidade {
-    //     $stmt = $this->pdo->prepare('SELECT * FROM habilidade WHERE id = :id LIMIT 1');
-    //     $stmt->execute([':id' => $id]);
-    //     $dados = $stmt->fetch();
+    public function buscarPorId(int $id): ?RelacaoPersonagemHabilidade {
+        $stmt = $this->pdo->prepare('SELECT * FROM personagem_habilidade WHERE id = :id LIMIT 1');
+        $stmt->execute([':id' => $id]);
+        $dados = $stmt->fetch();
 
-    //     if ($dados) {
-    //         return new Habilidade($dados);
-    //     }
+        if ($dados) {
+            return new RelacaoPersonagemHabilidade($dados);
+        }
 
-    //     return null;
-    // }
+        return null;
+    }
 
     public function salvar(RelacaoPersonagemHabilidade $relacao): void {
-        // if ($relacao->getId() > 0) {
-        //     $stmt = $this->pdo->prepare(
-        //         'UPDATE habilidade SET nome = :nome, tipo = :tipo, ciclo = :ciclo, estilo = :estilo, custo = :custo, descricao = :descricao WHERE id = :id'
-        //     );
-        //     $stmt->execute([
-        //         ':id' => $habilidade->getId(),
-        //         ':nome' => $habilidade->getNome(),
-        //         ':tipo' => $habilidade->getTipo(),
-        //         ':ciclo' => $habilidade->getCiclo(),
-        //         ':estilo' => $habilidade->getEstilo(),
-        //         ':custo' => $habilidade->getCusto(),
-        //         ':descricao' => $habilidade->getDescricao(),
-        //     ]);
-        //     return;
-        // }
+        if ($relacao->getId() > 0) {
+            $stmt = $this->pdo->prepare(
+                'UPDATE personagem_habilidade SET id_personagem = :id_personagem, id_habilidade = :id_habilidade WHERE id = :id'
+            );
+            $stmt->execute([
+                ':id' => $relacao->getId(),
+                ':id_personagem' => $relacao->getId_personagem(),
+                ':id_habilidade' => $relacao->getId_habilidade(),
+            ]);
+            return;
+        }
 
-        // if ($habilidade->getId_usuario() <= 0) {
-        //     throw new InvalidArgumentException('Usuário inválido.');
-        // }
+        if ($relacao->getId_usuario() <= 0) {
+            throw new InvalidArgumentException('Usuário inválido.');
+        }
 
         $stmt = $this->pdo->prepare(
-            'INSERT INTO personagem_habilidade (id_personagem, id_habilidade) VALUES (:id_personagem, :id_habilidade)'
+            'INSERT INTO personagem_habilidade (id_usuario, id_personagem, id_habilidade) VALUES (:id_usuario, :id_personagem, :id_habilidade)'
         );
         $stmt->execute([
+            ':id_usuario' => $relacao->getId_usuario(),
             ':id_personagem' => $relacao->getId_personagem(),
             ':id_habilidade' => $relacao->getId_habilidade()
         ]);
 
-        // $relacao->registrarIdGerado((int) $this->pdo->lastInsertId());
+        $relacao->registrarIdGerado((int) $this->pdo->lastInsertId());
     }
 
     public function inserir(int $id_personagem, int $id_habilidade): void {
-        $relacao = RelacaoPersonagemHabilidade::novo($id_personagem, $id_habilidade);
+        $relacao = RelacaoPersonagemHabilidade::novo($id_usuario, $id_personagem, $id_habilidade);
         $this->salvar($relacao);
     }
 
